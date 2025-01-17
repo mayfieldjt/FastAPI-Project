6d8ebfe307ac704154ea53fdc01b487f1bb6218a
mayfieldjt <78752966+mayfieldjt@users.noreply.github.com>
2025-01-17 01:17:19 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      external: ["plotly.js"], 
+    },
+  },
+  optimizeDeps: {
+    include: ["plotly.js-basic-dist"], 
+  },
 })
